37b8e34e07992dc3e6dda7f623a456d2ee2e27eb
gerard1001 <rutayisiregerard2002@gmail.com>
2024-09-13 09:25:56 +0200

    Certificates validation

--- src/modules/certificate/validations/certificate.validation.ts
+++ src/modules/certificate/validations/certificate.validation.ts
@@ -1,6 +1,6 @@
 import * as Joi from 'joi';
 
 export const createCertificateValidation = Joi.object({
-  title: Joi.string().required().empty().max(40),
+  title: Joi.string().required().empty().max(100),
   description: Joi.string().optional().empty().max(255),
 });
