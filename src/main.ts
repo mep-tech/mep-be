661e51a409f27a2a164571ae4bbe921578c8fe80
Tsa2341 <83913153+Tsa2341@users.noreply.github.com>
2025-10-09 20:04:33 +0200

    Fixed www. subdomain cors issues

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     },
   });
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://meperictrictech.com'],
+    origin: ['http://localhost:3000', 'https://meperictrictech.com', 'https://www.meperictrictech.com'],
   });
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
